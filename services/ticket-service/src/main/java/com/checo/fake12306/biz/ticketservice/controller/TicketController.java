4309d66fe39b770923a6cba3c19430ef5312f9f9
Checo Chan <xzchan168@gmail.com>
2024-02-21 01:10:56 +0800

    initial commit

---
+++ services/ticket-service/src/main/java/com/checo/fake12306/biz/ticketservice/controller/TicketController.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.checo.fake12306.biz.ticketservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import com.checo.fake12306.biz.ticketservice.dto.req.CancelTicketOrderReqDTO;
+import com.checo.fake12306.biz.ticketservice.dto.req.PurchaseTicketReqDTO;
+import com.checo.fake12306.biz.ticketservice.dto.req.RefundTicketReqDTO;
+import com.checo.fake12306.biz.ticketservice.dto.req.TicketPageQueryReqDTO;
+import com.checo.fake12306.biz.ticketservice.dto.resp.RefundTicketRespDTO;
+import com.checo.fake12306.biz.ticketservice.dto.resp.TicketPageQueryRespDTO;
+import com.checo.fake12306.biz.ticketservice.dto.resp.TicketPurchaseRespDTO;
+import com.checo.fake12306.biz.ticketservice.remote.dto.PayInfoRespDTO;
+import com.checo.fake12306.biz.ticketservice.service.TicketService;
+import com.checo.fake12306.framework.starter.convention.result.Result;
+import com.checo.fake12306.framework.starter.idempotent.annotation.Idempotent;
+import com.checo.fake12306.framework.starter.idempotent.enums.IdempotentSceneEnum;
+import com.checo.fake12306.framework.starter.idempotent.enums.IdempotentTypeEnum;
+import com.checo.fake12306.framework.starter.log.annotation.ILog;
+import com.checo.fake12306.framework.starter.web.Results;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 车票控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    /**
+     * 根据条件查询车票
+     */
+    @GetMapping("/api/ticket-service/ticket/query")
+    public Result<TicketPageQueryRespDTO> pageListTicketQuery(TicketPageQueryReqDTO requestParam) {
+        return Results.success(ticketService.pageListTicketQueryV1(requestParam));
+    }
+
+    /**
+     * 购买车票
+     */
+    @ILog
+    @Idempotent(
+            uniqueKeyPrefix = "fake12306-ticket:lock_purchase-tickets:",
+            key = "T(com.checo.fake12306.framework.starter.bases.ApplicationContextHolder).getBean('environment').getProperty('unique-name', '')"
+                    + "+'_'+"
+                    + "T(com.checo.fake12306.framework.starter.user.core.UserContext).getUsername()",
+            message = "正在执行下单流程，请稍后...",
+            scene = IdempotentSceneEnum.RESTAPI,
+            type = IdempotentTypeEnum.SPEL
+    )
+    @PostMapping("/api/ticket-service/ticket/purchase")
+    public Result<TicketPurchaseRespDTO> purchaseTickets(@RequestBody PurchaseTicketReqDTO requestParam) {
+        return Results.success(ticketService.purchaseTicketsV1(requestParam));
+    }
+
+    /**
+     * 购买车票 v2
+     */
+    @ILog
+    @Idempotent(
+            uniqueKeyPrefix = "fake12306-ticket:lock_purchase-tickets:",
+            key = "T(com.checo.fake12306.framework.starter.bases.ApplicationContextHolder).getBean('environment').getProperty('unique-name', '')"
+                    + "+'_'+"
+                    + "T(com.checo.fake12306.framework.starter.user.core.UserContext).getUsername()",
+            message = "正在执行下单流程，请稍后...",
+            scene = IdempotentSceneEnum.RESTAPI,
+            type = IdempotentTypeEnum.SPEL
+    )
+    @PostMapping("/api/ticket-service/ticket/purchase/v2")
+    public Result<TicketPurchaseRespDTO> purchaseTicketsV2(@RequestBody PurchaseTicketReqDTO requestParam) {
+        return Results.success(ticketService.purchaseTicketsV2(requestParam));
+    }
+
+    /**
+     * 取消车票订单
+     */
+    @ILog
+    @PostMapping("/api/ticket-service/ticket/cancel")
+    public Result<Void> cancelTicketOrder(@RequestBody CancelTicketOrderReqDTO requestParam) {
+        ticketService.cancelTicketOrder(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 支付单详情查询
+     */
+    @GetMapping("/api/ticket-service/ticket/pay/query")
+    public Result<PayInfoRespDTO> getPayInfo(@RequestParam(value = "orderSn") String orderSn) {
+        return Results.success(ticketService.getPayInfo(orderSn));
+    }
+
+    /**
+     * 公共退款接口
+     */
+    @PostMapping("/api/ticket-service/ticket/refund")
+    public Result<RefundTicketRespDTO> commonTicketRefund(@RequestBody RefundTicketReqDTO requestParam) {
+        return Results.success(ticketService.commonTicketRefund(requestParam));
+    }
+}
